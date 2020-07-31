b7b45012c4db6013504306e22822ba14b4f073b5
nicoledbianchin <nicoledbianchin@gmail.com>
2020-07-30 23:32:06 -0300

    back to stable version

--- src/test/java/com/atendoemlibras/api/ApiApplicationTests.java
+++ src/test/java/com/atendoemlibras/api/ApiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.atendoemlibras.api;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ApiApplicationTests {
 
 	@Test
