134fda2a599ff30e50884f326133af72c73046be
Andressa <neumm.andressa@gmail.com>
2020-08-11 16:20:33 -0300

    amend

--- src/main/java/com/atendoemlibras/api/domain/Professional.java
+++ src/main/java/com/atendoemlibras/api/domain/Professional.java
@@ -12,6 +12,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Entity
 @Table(name="professionals")
 public class Professional {
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
